5adb9e65a53c125dbc0a4956221ab3b47a6520a1
Abbey <abbeysantos1210@gmail.com>
2025-04-20 22:58:31 +0800

    chore: add variables fundamentals and activity

---
+++ src/java_fundamentals/VariablesAndDataTypes.java
@@ -0,0 +1,19 @@
+package java_fundamentals;
+
+public class VariablesAndDataTypes {
+
+	public static void main(String[] args) {
+		
+		String name = "Abbey";
+		
+		System.out.println(name);
+		
+		name = "Santos"; //Reassigning variables
+		
+		System.out.println(name);
+		
+		//Concatenation can be implemented in the variable or in the Print Statement
+		
+	}
+
+}
---
+++ src/java_fundamentals_activity/sentence_builder.java
@@ -0,0 +1,19 @@
+package java_fundamentals_activity;
+
+public class sentence_builder {
+
+	public static void main(String[] args) {
+		
+		String name = "Abbey";
+		int age = 23;
+		float gpa = 2.25f;
+		char bloodType = 'A';
+		
+		System.out.println("Hi my name is " + name);
+		System.out.println("I'am " + age + " years old");
+		System.out.println("My GPA is " + gpa);
+		System.out.println("My blood type is " + bloodType);
+		
+	}
+
+}
