74bd824b5df6924ee190c8c6bd65b8e4939a79e1
Abbey <abbeysantos1210@gmail.com>
2025-05-08 19:31:16 +0800

    chore: add switch statement lesson

---
+++ src/java_fundamentals/SwitchStatements.java
@@ -0,0 +1,27 @@
+package java_fundamentals;
+
+public class SwitchStatements {
+
+	public static void main(String[] args) {
+		
+		char grade = 'A';
+		
+		switch(grade) {
+		
+			case 'A':
+				System.out.println("Outstanding");
+				break;
+			case 'B':
+				System.out.println("Excellent");
+				break;
+			case 'C':
+				System.out.println("Satisfactory");
+				break;
+				
+			default:
+				System.out.println("Invalid Grade");
+		}
+
+	}
+
+}
